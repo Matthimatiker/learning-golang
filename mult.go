09bb425c0f9a945037124d3816184eaa163b2147
Matthias Molitor <mmo@webfactory.de>
2017-04-11 18:38:49 +0200

    print multiplication table without formatting

--- mult.go
+++ mult.go
@@ -1,14 +1,31 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	n := int(os.Args[1])
+	n, err := strconv.Atoi(os.Args[1])
+	if (err != nil) {
+		panic(err)
+	}
 	mult(n)
 }
 
 func mult(n int) {
-	for i := 0; i < n; i++ {
+	fmt.Print(" ")
+	for i := 1; i <= n; i++ {
+		fmt.Print(i, " ")
+	}
+	fmt.Println()
 
+	for i := 1; i <= n; i++ {
+		fmt.Print(i, " ")
+		for j := 1; j <= n; j++ {
+			fmt.Print(i * j, " ")
+		}
+		fmt.Println()
 	}
 }
