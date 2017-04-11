a899d98c2b198762186f7b07a63b23e1fe8518e3
Matthias Molitor <mmo@webfactory.de>
2017-04-11 23:41:22 +0200

    reverse lines before printing them

--- tac/tac.go
+++ tac/tac.go
@@ -56,9 +56,18 @@ func main() {
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		fmt.Println(reverse(scanner.Text()))
 	}
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
+}
+
+func reverse(bytes string) string {
+	strLength := len(bytes);
+	reversed := make([]byte, strLength, strLength)
+	for i := 0; i < strLength; i++ {
+		reversed[i] = bytes[strLength - i - 1]
+	}
+	return string(reversed)
 }
