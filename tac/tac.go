e37814dc5113e3daab0a98832cac88e17eef42db
Matthias Molitor <mmo@webfactory.de>
2017-04-11 21:59:45 +0200

    created reader that should read file chunks in reverse order (every chunk is currently in ascending order though)

--- tac/tac.go
+++ tac/tac.go
@@ -1,11 +1,42 @@
 package main
 
 import (
-	//"bufio"
 	"fmt"
 	"os"
 	"bufio"
+	"io"
 )
+
+type ReverseReader struct {
+	file *os.File;
+	pos int64
+}
+
+func NewReverseReader(file *os.File) *ReverseReader {
+	info, err := file.Stat()
+	if (err != nil) {
+		panic(err)
+	}
+	return &ReverseReader{
+		file: file,
+		pos: info.Size(),
+	}
+}
+
+func (reader *ReverseReader) Read(p []byte) (int, error) {
+	numberOfBytesToRead := len(p)
+	if (int(reader.pos) < numberOfBytesToRead) {
+		// Not enough bytes left, read less
+		numberOfBytesToRead = int(reader.pos)
+	}
+	if numberOfBytesToRead == 0 {
+		// No more bytes to read. Send io.EOF to indicate end of file.
+		return 0, io.EOF
+	}
+	reader.pos -= int64(numberOfBytesToRead)
+	_, err := reader.file.ReadAt(p, reader.pos)
+	return numberOfBytesToRead, err
+}
 
 func main() {
 	filePath := os.Args[1]
@@ -15,7 +46,7 @@ func main() {
 	}
 	defer file.Close()
 
-	reader := bufio.NewReader(file)
+	reader := NewReverseReader(file)
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
