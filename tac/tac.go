69902142698d34866cc691bac5cb6576f87d3b98
Matthias Molitor <mmo@webfactory.de>
2017-04-11 23:51:29 +0200

    moved ReverseReader to separate file

--- tac/tac.go
+++ tac/tac.go
@@ -3,46 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"github.com/matthimatiker/learning-golang/util"
 )
-
-type ReverseReader struct {
-	file *os.File
-	pos  int64
-}
-
-func NewReverseReader(file *os.File) *ReverseReader {
-	info, err := file.Stat()
-	if err != nil {
-		panic(err)
-	}
-	return &ReverseReader{
-		file: file,
-		pos:  info.Size(),
-	}
-}
-
-func (reader *ReverseReader) Read(p []byte) (int, error) {
-	numberOfBytesToRead := len(p)
-	if int(reader.pos) < numberOfBytesToRead {
-		// Not enough bytes left, read less
-		numberOfBytesToRead = int(reader.pos)
-	}
-	if numberOfBytesToRead == 0 {
-		// No more bytes to read. Send io.EOF to indicate end of file.
-		return 0, io.EOF
-	}
-	reader.pos -= int64(numberOfBytesToRead)
-	// Read the chunk...
-	buffer := make([]byte, numberOfBytesToRead, numberOfBytesToRead)
-	_, err := reader.file.ReadAt(buffer, reader.pos)
-	// ...and pass provide the bytes in reverse order.
-	for i := 0; i < numberOfBytesToRead; i++ {
-		p[i] = buffer[numberOfBytesToRead-i-1]
-	}
-	return numberOfBytesToRead, err
-}
 
 func main() {
 	filePath := os.Args[1]
@@ -52,7 +15,7 @@ func main() {
 	}
 	defer file.Close()
 
-	reader := NewReverseReader(file)
+	reader := ReverseReader.NewReverseReader(file)
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
