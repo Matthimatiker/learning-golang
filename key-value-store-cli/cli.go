9df4d469ed5eda5cf6cc85de285feb7bc52188ef
Matthias Molitor <mmo@webfactory.de>
2017-04-30 20:35:13 +0200

    implemented CLI interface that store the key value file in the temp directory

--- key-value-store-cli/cli.go
+++ key-value-store-cli/cli.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"os"
+	"github.com/matthimatiker/learning-golang/key-value-store"
+	"fmt"
+)
 
+func main() {
+	store, err := key_value_store.NewStore(getStoreFile())
+	if (err != nil) {
+		panic(err)
+	}
+	for _, arg :=range os.Args[1:] {
+		key, value := key_value_store.ToKeyValue(arg)
+		if (key_value_store.IsKeyValuePair(arg)) {
+			store.Set(key, value)
+
+		} else {
+			value = store.Get(key)
+		}
+		fmt.Printf("> %s = %s", key, value)
+		fmt.Println()
+	}
+}
+
+func getStoreFile() string {
+	return os.TempDir() + "/learning-golang-default.store"
 }
