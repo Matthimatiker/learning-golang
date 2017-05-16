4eea12a43dad4c0d153e8020d689f031754e5310
Matthias Molitor <mmo@webfactory.de>
2017-05-16 11:39:04 +0200

    changed structure; implemented tests

--- key-value-store/server.go
+++ key-value-store/server.go
@@ -2,11 +2,17 @@ package key_value_store
 
 import "net/http"
 
-type StoreHandler http.Handler
+type storeHandler struct {
+	store KeyValueStore
+}
 
 // Returns a HTTP handler that operates on the given key-value store.
-func NewStoreHandler(store KeyValueStore) StoreHandler {
-	return func(w http.ResponseWriter, r *http.Request) {
+func NewStoreHandler(store KeyValueStore) http.Handler {
+	return &storeHandler{
+		store: store,
+	}
+}
 
-	};
+func (handler *storeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 }
--- key-value-store/server_test.go
+++ key-value-store/server_test.go
@@ -3,29 +3,92 @@ package key_value_store
 import (
 	"testing"
 	"github.com/sensu/uchiwa/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"strings"
+	"net/http"
 )
 
-func setUp() (StoreHandler, KeyValueStore) {
-
+func setUp() (http.Handler, KeyValueStore) {
+	store := newInMemoryStore()
+	return NewStoreHandler(store), store
 }
 
 func Test_ReturnsCode201OnCreation(t *testing.T) {
+	handler, _ := setUp()
 
-	assert.New(t).NotNil(nil)
+	req := httptest.NewRequest("POST", "/my-key", strings.NewReader("my-value"))
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
 }
 
 func Test_AddsKeyToStore(t *testing.T) {
+	handler, store := setUp()
 
+	req := httptest.NewRequest("POST", "/my-key", strings.NewReader("my-value"))
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, "my-value", store.Get("my-key"))
 }
 
 func Test_ReturnsCode200WhenFound(t *testing.T) {
+	handler, store := setUp()
+	store.Set("my-key", "my-value");
 
+	req := httptest.NewRequest("GET", "/my-key", nil)
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
 }
 
 func Test_ReturnsValueInResponse(t *testing.T) {
+	handler, store := setUp()
+	store.Set("my-key", "my-value");
 
+	req := httptest.NewRequest("GET", "/my-key", nil)
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, "my-value", w.Body.String())
 }
 
 func Test_ReturnsCode404IfNotExists(t *testing.T) {
+	handler, _ := setUp()
 
+	req := httptest.NewRequest("GET", "/missing-key", nil)
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+}
+
+type inMemoryStore struct {
+	data map[string]string
+}
+
+// Creates an empty in-memory key-value store.
+func newInMemoryStore() (*inMemoryStore) {
+	return &inMemoryStore{
+		data: map[string]string{},
+	}
+}
+
+func (store *inMemoryStore) Get(key string) string {
+	return store.data[key];
+}
+
+func (store *inMemoryStore) Set(key string, value string) {
+	store.data[key] = value;
+}
+
+func (store *inMemoryStore) All() (map[string]string) {
+	return store.data;
 }
