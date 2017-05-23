8be7bbe541b4128cb8598c7ac1750da8e7c0f07b
Matthias Molitor <mmo@webfactory.de>
2017-05-23 12:30:30 +0200

    implemented web client

--- key-value-store/web_client.go
+++ key-value-store/web_client.go
@@ -1,19 +1,31 @@
 package key_value_store
 
+import (
+	"strings"
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
 type webClient struct {
 	url string
 }
 
 func NewWebClient(url string) (*webClient) {
 	return &webClient{
-		url: url,
+		url: strings.TrimSuffix(url, "/"),
 	}
 }
 
-func (store *webClient) Get(key string) string {
-
+func (client *webClient) Get(key string) string {
+	response, err := http.Get(client.url + "/" + key)
+	assertNoError(err)
+	value, err := ioutil.ReadAll(response.Body)
+	assertNoError(err)
+	return string(value)
 }
 
-func (store *webClient) Set(key string, value string) {
-
+func (client *webClient) Set(key string, value string) {
+	_, err := http.Post(client.url + "/" + key, "text/plain", bytes.NewBufferString(value))
+	assertNoError(err)
 }
