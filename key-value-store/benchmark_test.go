91b08614441125f31c9646dc871d314d3043222c
Matthias Molitor <mmo@webfactory.de>
2017-05-26 21:50:25 +0200

    created some test stubs

---
+++ key-value-store/benchmark_test.go
@@ -0,0 +1,30 @@
+package key_value_store
+
+import "testing"
+
+func Test_BenchmarkConfigurationProvidesSensibleDefaultValues(t *testing.T) {
+
+}
+
+func Test_BenchmarkConfigurationCanBeConfigured(t *testing.T) {
+
+}
+
+func Test_BenchmarkExecutesCorrectOfOperationsInCaseOfSequentialExecution(t *testing.T) {
+
+}
+
+func Test_BenchmarkExecutesCorrectNumberOfOperationsInCaseOfParallelExecution(t *testing.T) {
+
+}
+
+
+func Test_ParallelExecutionIsFasterThanSequential(t *testing.T) {
+
+}
+
+func Test_BenchmarkReturnsValidResult(t *testing.T) {
+
+}
+
+
