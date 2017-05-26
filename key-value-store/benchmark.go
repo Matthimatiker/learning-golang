70d444d58688841fdaaf10671e3b0234b2b4b95d
Matthias Molitor <mmo@webfactory.de>
2017-05-26 18:15:15 +0200

    design refactoring: extracted configuration object

--- key-value-store/benchmark.go
+++ key-value-store/benchmark.go
@@ -5,6 +5,33 @@ type benchmark struct {
 	operations chan func(SimpleKeyValueStore)
 }
 
+// Encapsulates a configuration for a benchmark run.
+type benchmarkRunConfiguration struct {
+	numberOfOperations int
+	parallelOperations int
+	writeOperationRatio float32
+}
+
+// Creates a new benchmark configuration with default values.
+func NewBenchmarkRunConfiguration() benchmarkRunConfiguration {
+
+}
+
+// Sets the number of operations that will be executed and returns the new configuration.
+func (config benchmarkRunConfiguration) NumberOfOperations(value int) benchmarkRunConfiguration {
+
+}
+
+// Sets the number of operations that will be executed in parallel and returns the new configuration.
+func (config benchmarkRunConfiguration) ParallelOperations(value int) benchmarkRunConfiguration {
+
+}
+
+// Defines the number of write operations among the whole number of operations.
+// value must be a float between 0.0 and 1.0.
+func (config benchmarkRunConfiguration) WriteOperationRatio(value float32) benchmarkRunConfiguration {
+
+}
 
 func NewBenchmark(store SimpleKeyValueStore) *benchmark {
 
@@ -12,6 +39,6 @@ func NewBenchmark(store SimpleKeyValueStore) *benchmark {
 
 // Runs a benchmark against the store.
 //
-func (benchmark *benchmark) run(numberOfOperations int, parallelOperations int, writeOperationRatio float32) float64 {
+func (benchmark *benchmark) run(config benchmarkRunConfiguration) float64 {
 
 }
