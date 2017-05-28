0eee32e1c0241c98a04a57360c1f24f7d89c9c33
Matthias Molitor <mmo@webfactory.de>
2017-05-28 16:49:41 +0200

    used locks to make in-memory-store thread safe

--- key-value-store/in_memory_store.go
+++ key-value-store/in_memory_store.go
@@ -1,21 +1,29 @@
 package key_value_store
 
+import "sync"
+
 type inMemoryStore struct {
 	data map[string]string
+	lock *sync.RWMutex
 }
 
 // Creates an empty in-memory key-value store.
 func NewInMemoryStore() (KeyValueStore) {
 	return &inMemoryStore{
 		data: map[string]string{},
+		lock: &sync.RWMutex{},
 	}
 }
 
 func (store *inMemoryStore) Get(key string) string {
+	store.lock.RLock()
+	defer store.lock.RUnlock()
 	return store.data[key];
 }
 
 func (store *inMemoryStore) Set(key string, value string) {
+	store.lock.Lock()
+	defer store.lock.Unlock()
 	store.data[key] = value;
 }
 
