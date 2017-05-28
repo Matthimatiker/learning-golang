404e0ede1a8fa470aaba4357d902493239ca5a40
Matthias Molitor <mmo@webfactory.de>
2017-05-28 16:58:50 +0200

    created scenario list (to be able to register multiple stores for benchmark)

--- key-value-store-benchmark/benchmark.go
+++ key-value-store-benchmark/benchmark.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
-	store := key_value_store.NewInMemoryStore()
-	benchmark := key_value_store.NewBenchmark(store)
-
+	scenarios := [1]struct{
+		headline string
+		store key_value_store.SimpleKeyValueStore
+	}{
+		{
+			headline: "# InMemoryStore",
+			store: key_value_store.NewInMemoryStore(),
+		},
+	}
 	configurations := [2]struct {
 		headline string
 		config   key_value_store.BenchmarkRunConfiguration
@@ -22,9 +28,14 @@ func main() {
 			config:   key_value_store.NewBenchmarkRunConfiguration().NumberOfOperations(10000).ParallelOperations(10).WriteOperationRatio(0.2),
 		},
 	}
-	for _, configuration := range configurations {
-		fmt.Print(configuration.headline + "\n")
-		result := benchmark.Run(configuration.config)
-		fmt.Print(result.String() + "\n")
+
+	for _, scenario := range scenarios {
+		fmt.Print(scenario.headline + "\n\n")
+		benchmark := key_value_store.NewBenchmark(scenario.store)
+		for _, configuration := range configurations {
+			fmt.Print(configuration.headline + "\n")
+			result := benchmark.Run(configuration.config)
+			fmt.Print(result.String() + "\n")
+		}
 	}
 }
