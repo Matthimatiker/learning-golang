267e35fd375154a83506f954d82beaf7435d9cd9
Matthias Molitor <mmo@webfactory.de>
2017-05-28 16:53:57 +0200

    added headlines for applied configurations

--- key-value-store-benchmark/benchmark.go
+++ key-value-store-benchmark/benchmark.go
@@ -11,12 +11,22 @@ func main() {
 	sequential := key_value_store.NewBenchmarkRunConfiguration().NumberOfOperations(10000).ParallelOperations(1).WriteOperationRatio(0.2)
 	parallel := key_value_store.NewBenchmarkRunConfiguration().NumberOfOperations(10000).ParallelOperations(10).WriteOperationRatio(0.2)
 
-	configs := [2]key_value_store.BenchmarkRunConfiguration{
-		sequential,
-		parallel,
+	configurations := [2]struct{
+		headline string
+		config key_value_store.BenchmarkRunConfiguration
+	}{
+		{
+			headline: "## Sequential",
+			config: sequential,
+		},
+		{
+			headline: "## Parallel",
+			config: parallel,
+		},
 	}
-	for _, config := range configs {
-		result := benchmark.Run(config)
+	for _, configuration := range configurations {
+		fmt.Print(configuration.headline + "\n")
+		result := benchmark.Run(configuration.config)
 		fmt.Print(result.String() + "\n")
 	}
 }
