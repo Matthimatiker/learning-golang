5e1ea2e956d47199445b91283f1e2c3dbf510d8a
Matthias Molitor <mmo@webfactory.de>
2017-05-28 16:44:23 +0200

    execute sequential and parallel benchmark

--- key-value-store-benchmark/benchmark.go
+++ key-value-store-benchmark/benchmark.go
@@ -9,6 +9,14 @@ func main() {
 	store := key_value_store.NewInMemoryStore()
 	benchmark := key_value_store.NewBenchmark(store)
 	sequential := key_value_store.NewBenchmarkRunConfiguration().NumberOfOperations(10000).ParallelOperations(1).WriteOperationRatio(0.2)
-	result := benchmark.Run(sequential)
-	fmt.Print(result.String())
+	parallel := key_value_store.NewBenchmarkRunConfiguration().NumberOfOperations(10000).ParallelOperations(10).WriteOperationRatio(0.2)
+
+	configs := [2]key_value_store.BenchmarkRunConfiguration{
+		sequential,
+		parallel,
+	}
+	for _, config := range configs {
+		result := benchmark.Run(config)
+		fmt.Print(result.String() + "\n")
+	}
 }
